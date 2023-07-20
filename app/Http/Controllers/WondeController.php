99c64c2141fb26ee33bad478b8d5c8ed23ac5f21
maxwkf <maxwkf@gmail.com>
2023-07-20 23:03:49 +0100

    page to generate result

--- app/Http/Controllers/WondeController.php
+++ app/Http/Controllers/WondeController.php
@@ -2,14 +2,42 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class WondeController extends Controller
 {
-    public function show(\Wonde\Client $client) {
+    private $_client;
+    private $_school;
 
-        
-        dd($client->schools->all());
-        return view('wonde');
+    public function __construct(\Wonde\Client $client) {
+        $this->_client = $client;
+        $this->_school = $this->_client->school(env('WONDE_SCHOOL_ID'));
     }
+    public function show() {
+
+        // get all employess for generation of selection box
+        $allEmployees = $this->_school->employees->all();
+
+        // if no employeeId is set, return without results
+        if (!request()->has('employeeId')) {
+            return view('wonde', [
+                'allEmployees' => $allEmployees
+            ]);
+        }
+
+        // get employee, A500460806
+        $targetEmployee = $this->_school->employees->get(request()->get('employeeId'), ['classes']);
+        
+        // get classes from employee
+        $classes = array_reduce($targetEmployee->classes->data, function($carry, $class) {
+            $carry[] = $this->_school->classes->get($class->id, ['students']);
+            return $carry;
+        }, []);
+
+        return view('wonde', [
+            'allEmployees' => $allEmployees,
+            'targetEmployee' => $targetEmployee,
+            'classes' => $classes
+        ]);
+    }
+
+    
 }
--- resources/views/wonde.blade.php
+++ resources/views/wonde.blade.php
@@ -1,1 +1,42 @@
-hi
+<!-- create a form -->
+<form method="GET" action="/wonde">
+    @csrf
+    Employee: 
+    <select name="employeeId">
+        @if ($allEmployees)
+
+        @foreach($allEmployees as $employee)
+            <option value="{{ $employee->id }}" {{ $targetEmployee == $employee->id ? 'selected="selected"' : '' }}>{{ $employee->title }} {{ $employee->forename }} {{ $employee->surname }}</option>
+        @endforeach
+
+    @endif
+    </select>
+    <button type="submit">Submit</button>
+</form>
+@isset($targetEmployee)
+    @if($classes)
+        @foreach ($classes as $class)
+            <h1>Class: {{ $class->name }}</h1>
+            @if($class->students->data)
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Student</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($class->students->data as $student)
+                            <tr>
+                                <td>{{ $student->forename }} {{ $student->surname }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @else
+                <h1>No students found</h1>
+            @endif
+        @endforeach
+    @else
+        <h1>No classes found</h1>
+    @endif
+@endisset
